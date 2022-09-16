29a3556b3e826b65d524c99d3368aaea043092b6
Guilherme Amorim <guilherme.asfreitas@gmail.com>
2022-09-16 19:50:54 +0100

    Add Exerc01. class 20

---
+++ src/com/loiane/cursojava/exercicios_aula20/Exerc01.java
@@ -0,0 +1,38 @@
+package com.loiane.cursojava.exercicios_aula20;
+
+import java.util.Random;
+
+public class Exerc01 {
+
+	public static void main(String[] args) {
+
+		int[][] values = new int[4][4];
+		int maxNumber = Short.MIN_VALUE;
+		int line = 0;
+		int col = 0;
+
+		Random randomNumber = new Random();
+
+		
+		for (int i = 0; i < values.length; i++) {
+			System.out.print("L[" + i+ "]: ");
+			for (int j = 0; j < values[i].length; j++) {
+				values[i][j] = randomNumber.nextInt(100);
+				System.out.print(values[i][j] + " ");
+
+				if (maxNumber < values[i][j]) {
+					maxNumber = values[i][j];
+					line = i;
+					col = j;
+				}
+			}
+			System.out.println();
+
+		}
+
+		System.out.println("\nMax Number: " + maxNumber);
+		System.out.println("Line: " + line);
+		System.out.println("Column: " + col);
+	}
+
+}
